714cc589d6e907b598284c8ce709d4eb4b53e9cf
pgermishuys <pieter@pieterg.com>
2013-07-30 05:26:16 +0200

    cleaned up the configuration builder

--- src/Jubilee.Core/ConfigurationBuilder.cs
+++ src/Jubilee.Core/ConfigurationBuilder.cs
@@ -13,50 +13,36 @@ namespace Jubilee.Core
 {
 	public class ConfigurationBuilder
 	{
-		private List<Type> plugins;
-		private List<Type> notificationPlugins;
-		private Type runner;
 		private IKernel kernel;
 		public ConfigurationBuilder()
 		{
-			plugins = new List<Type>();
-			notificationPlugins = new List<Type>();
 			kernel = new StandardKernel();
 		}
 		public ConfigurationBuilder ScanAssembliesForPlugins(string[] assemblyNames)
 		{
 			foreach (var assemblyName in assemblyNames)
 			{
-				
+
 			}
 			return this;
 		}
 		public ConfigurationBuilder WithPlugin<T>() where T : IPlugin
 		{
-			this.plugins.Add(typeof(T));
+			kernel.Bind<IPlugin>().To(typeof(T));
 			return this;
 		}
 		public ConfigurationBuilder WithNotificationPlugin<T>() where T : INotificationPlugin
 		{
-			this.notificationPlugins.Add(typeof(T));
+			kernel.Bind<INotificationPlugin>().To(typeof(T));
 			return this;
 		}
 		public ConfigurationBuilder WithRunner<T>() where T : IRunner
 		{
-			this.runner = typeof(T); 
+			kernel.Bind<IRunner>().To(typeof(T)); 
 			return this;
 		}
 		public IRunner Build()
 		{
-			foreach (var plugin in plugins)
-			{
-				kernel.Bind<IPlugin>().To(plugin);
-			}
-			foreach (var notificationPlugin in notificationPlugins)
-			{
-				kernel.Bind<INotificationPlugin>().To(notificationPlugin);
-			}
-			kernel.Bind<IRunner>().To(runner);
 			kernel.Bind<INotificationService>().To<NotificationService>();
 			return kernel.Get<IRunner>();
 		}
