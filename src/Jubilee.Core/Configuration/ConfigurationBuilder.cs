0dcff5f56a0ac777ae503abc75afdcc94a3a121a
pgermishuys <pieter@pieterg.com>
2013-08-01 05:42:12 +0200

    Removed redundant configuration methods

--- src/Jubilee.Core/Configuration/ConfigurationBuilder.cs
+++ src/Jubilee.Core/Configuration/ConfigurationBuilder.cs
@@ -30,7 +30,7 @@ namespace Jubilee.Core.Configuration
 			this.serializer = new Jubilee.Core.Serialization.YamlSerializer();
 			this.kernel = new StandardKernel();
 		}
-		public ConfigurationBuilder(string settingsFilePath) :this()
+		public ConfigurationBuilder(string settingsFilePath) : this()
 		{
 			if (!File.Exists(settingsFilePath))
 			{
@@ -68,37 +68,6 @@ namespace Jubilee.Core.Configuration
 		{
 			var notificationType = knownTypes.GetType(notificationConfiguration.Name);
 			kernel.Bind<INotificationPlugin>().To(notificationType);
-		}
-		public ConfigurationBuilder ScanAssembliesForPlugins(params string[] assemblyNames)
-		{
-			foreach (var assemblyName in assemblyNames)
-			{
-				if (File.Exists(assemblyName))
-				{
-					var loadedAssembly = Assembly.LoadFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, assemblyName));
-					var plugins = loadedAssembly.GetTypes().Where(x => typeof(IPlugin).IsAssignableFrom(x) && x.IsClass);
-					foreach (var plugin in plugins)
-					{
-						kernel.Bind<IPlugin>().To(plugin);
-					}
-				}
-			}
-			return this;
-		}
-		public ConfigurationBuilder WithPlugin<T>() where T : IPlugin
-		{
-			kernel.Bind<IPlugin>().To(typeof(T));
-			return this;
-		}
-		public ConfigurationBuilder WithNotificationPlugin<T>() where T : INotificationPlugin
-		{
-			kernel.Bind<INotificationPlugin>().To(typeof(T));
-			return this;
-		}
-		public ConfigurationBuilder WithRunner<T>() where T : IRunner
-		{
-			kernel.Bind<IRunner>().To(typeof(T));
-			return this;
 		}
 		public IRunner Build()
 		{
