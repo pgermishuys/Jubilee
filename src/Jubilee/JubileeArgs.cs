d579bafd91dfd680f5f16f81bcd66cd6570eba15
pgermishuys <pieter@pieterg.com>
2014-01-15 04:52:08 +0200

    Removed the incorrect information about running Jubilee

--- src/Jubilee/JubileeArgs.cs
+++ src/Jubilee/JubileeArgs.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace Jubilee
 {
-	[ArgExample("jubilee -w c:/PathToWatch", "asks jubilee to watch the given path for changes and kick off the workflow defined in the configuration.yaml")]
+    [ArgExample("Jubilee.exe", "runs Jubilee using workflow defined in the configuration.yaml")]
 	public class JubileeArgs
 	{
 		[ArgShortcut("?")]
-		[ArgDescription("displays the jubilee help.")]
+        [ArgDescription("displays the Jubilee help.")]
 		public bool Help { get; set; }
 
 		[ArgShortcut("s")]
-		[ArgDescription("scaffolds a new scriptcs jubilee plugin.")]
+        [ArgDescription("scaffolds a new scriptcs Jubilee plugin.")]
 		public string Scaffold { get; set; }
 	}
 }
